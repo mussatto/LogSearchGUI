1fae462ac19575e5a74c3e8130c22cef883d9afa
Vitor Mussatto <mussatto@gmail.com>
2015-08-10 21:52:19 -0300

    setting actions

--- src/com/curupira/logsearch/LoginSearchControl.java
+++ src/com/curupira/logsearch/LoginSearchControl.java
@@ -1,10 +1,35 @@
 package com.curupira.logsearch;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 public class LoginSearchControl extends VBox{
 
+    @FXML
+    private TextField searchQuery;
+
+    @FXML
+    private TextArea logResult;
+
+    @FXML
+    private TextField fileInputString;
+
+    @FXML
+    private CheckBox isInMemory;
+
+    @FXML
+    private TextField indexFileString;
+
+    public TextField getSearchQuery() {
+        return searchQuery;
+    }
+
+    public void setSearchQuery(TextField searchQuery) {
+        this.searchQuery = searchQuery;
+    }
 
     @FXML
     protected void doSearch() {
@@ -19,5 +44,6 @@ public class LoginSearchControl extends VBox{
     @FXML
     protected void doIndexFile(){
         System.out.println("Index File");
+        System.out.println("to"+indexFileString.getText());
     }
 }
